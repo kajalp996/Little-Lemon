dcbca02f49f64b477274a12c387d2ceeb67e2ffa
Kajal Bhadaniya <kajup772@gmail.com>
2024-03-08 13:27:59 -0500

    Commit Saved

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./Logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Company logo" />
+    </header>
+  );
+};
+
+export default Header;
